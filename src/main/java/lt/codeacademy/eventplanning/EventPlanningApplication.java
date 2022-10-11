a7bc0941110d52b03cc7f670acfcaefb8241acac
Vaidotak <pehtrauhk@gmail.com>
2022-10-11 21:27:03 +0300

    Pirmas sukėlima

---
+++ src/main/java/lt/codeacademy/eventplanning/EventPlanningApplication.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.eventplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlanningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/codeacademy/eventplanning/EventPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.eventplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
