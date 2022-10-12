1ea89e73284e92182727586922569fffa566d144
Vaidotak <pehtrauhk@gmail.com>
2022-10-12 15:28:12 +0300

    pataisytas neimingas

--- src/main/java/lt/codeacademy/eventplanning/controllers/UserRegistrationController.java
+++ src/main/java/lt/codeacademy/eventplanning/controllers/UserRegistrationController.java
@@ -1,6 +1,8 @@
 package lt.codeacademy.eventplanning.controllers;
 
 
+import lt.codeacademy.eventplanning.dto.GetUserRegistrationResponseDTO;
+import lt.codeacademy.eventplanning.dto.CreateUserRegistrationRequestDTO;
 import lt.codeacademy.eventplanning.entities.UserRegistration;
 import lt.codeacademy.eventplanning.services.UserRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/user-registration")
@@ -24,23 +25,28 @@ public class UserRegistrationController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserRegistration> getUserById(@PathVariable(name="id") Long id){
-        Optional<UserRegistration> user = this.userRegistrationService.getUsersById(id);
-        if(user.isPresent()){
-            return ResponseEntity.ok(user.get());
-        }else{
+    public ResponseEntity<GetUserRegistrationResponseDTO> getUserById(@PathVariable(name="id") Long id){
+        GetUserRegistrationResponseDTO user = this.userRegistrationService.getUsersById(id);
+
+        if(user == null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        else return ResponseEntity.ok(user);
     }
 
     @GetMapping("/{id}/birthday")
     public ResponseEntity<String> getUserBirtDayById(@PathVariable(name="id") Long id){
-        Optional<UserRegistration> user = this.userRegistrationService.getUsersById(id);
-        if(user.isPresent()){
-            return ResponseEntity.ok(user.get().getBirthDate());
-        }else{
+        GetUserRegistrationResponseDTO user = this.userRegistrationService.getUsersById(id);
+        if(user == null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        else return ResponseEntity.ok(user.getBirthDate());
+    }
+
+    @PostMapping
+    public void addUser(@RequestBody CreateUserRegistrationRequestDTO createUserRegistrationRequestDTO){
+        System.out.println(createUserRegistrationRequestDTO.toString());
+
     }
 
 }
