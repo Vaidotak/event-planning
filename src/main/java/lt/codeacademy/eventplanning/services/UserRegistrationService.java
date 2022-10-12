e75d9cbbaa799d23f7955239a6eae3eb3aaa0a42
Vaidotak <pehtrauhk@gmail.com>
2022-10-12 16:15:40 +0300

    reformat code

--- src/main/java/lt/codeacademy/eventplanning/services/UserRegistrationService.java
+++ src/main/java/lt/codeacademy/eventplanning/services/UserRegistrationService.java
@@ -1,9 +1,16 @@
 package lt.codeacademy.eventplanning.services;
 
 
+import lt.codeacademy.eventplanning.dto.GetUserRegistrationResponseDTO;
 import lt.codeacademy.eventplanning.entities.UserRegistration;
 import lt.codeacademy.eventplanning.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import static lt.codeacademy.eventplanning.converter.UserRegistrationConverter.convertUserRegistrationToGetUserRegistrationResponseDTO;
 
 @Service
 public class UserRegistrationService {
@@ -14,7 +21,22 @@ public class UserRegistrationService {
         this.userRepository = userRepository;
     }
 
-    public void addUser(UserRegistration userRegistration){
+    public List<UserRegistration> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public GetUserRegistrationResponseDTO getUsersById(Long id) {
+
+        Optional<UserRegistration> user = this.userRepository.findById(id);
+        if (user.isPresent()) {
+
+            return convertUserRegistrationToGetUserRegistrationResponseDTO(user.get());
+        } else {
+            return null;
+        }
+    }
+
+    public void addUser(UserRegistration userRegistration) {
         this.userRepository.save(userRegistration);
     }
 
