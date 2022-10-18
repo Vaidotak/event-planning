a0defdb1ad183e538f787fa214d60c2aa3a8645d
Vaidotak <pehtrauhk@gmail.com>
2022-10-18 10:39:04 +0300

    Patvarkytas scriptas

--- src/main/resources/static/modules/birth-to-age.js
+++ src/main/resources/static/modules/birth-to-age.js
@@ -4,7 +4,7 @@ export function getAge(dateString) {
     const age = today.getFullYear() - birthDate.getFullYear();
     const m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
+        age;
     }
     return age;
   }
